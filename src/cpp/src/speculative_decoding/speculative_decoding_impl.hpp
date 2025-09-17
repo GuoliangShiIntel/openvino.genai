c14ef579cc7de236e079be88d091da6b8029f077
GuoliangShiIntel <guoliang.shi@intel.com>
2025-09-17 23:48:46 +0800

    Fast draft support on npu (SDPA Backend) PR#2544

--- src/cpp/src/speculative_decoding/speculative_decoding_impl.hpp
+++ src/cpp/src/speculative_decoding/speculative_decoding_impl.hpp
@@ -8,6 +8,7 @@
 #include "speculative_decoding/continuous_batching_for_speculative_decoding_impl.hpp"
 #include "speculative_decoding/speculative_decoding_metrics.hpp"
 #include "openvino/genai/speculative_decoding/perf_metrics.hpp"
+#include "utils.hpp"
 
 #include "openvino/op/add.hpp"
 #include "openvino/op/constant.hpp"
@@ -23,30 +24,6 @@
 #include "openvino/pass/manager.hpp"
 
 namespace ov::genai {
-
-struct ModelDesc {
-    std::string device;
-    ov::genai::SchedulerConfig scheduler_config;
-    ov::AnyMap properties;
-    ov::genai::GenerationConfig generation_config;
-    std::shared_ptr<ov::Model> model = nullptr;
-    ov::genai::Tokenizer tokenizer;
-
-    ModelDesc(const std::shared_ptr<ov::Model>& model,
-              const ov::genai::Tokenizer& tokenizer,
-              const std::string& device = {},
-              const ov::AnyMap& properties = {},
-              const ov::genai::SchedulerConfig& scheduler_config = {},
-              const ov::genai::GenerationConfig& generation_config = {}) :
-        model(model),
-        tokenizer(tokenizer),
-        device(device),
-        properties(properties),
-        scheduler_config(scheduler_config),
-        generation_config(generation_config) {}
-    
-    ModelDesc() = default;
-};
 
 class ContinuousBatchingPipeline::SpeculativeDecodingImpl : public ContinuousBatchingPipeline::IContinuousBatchingPipeline {
 protected:
